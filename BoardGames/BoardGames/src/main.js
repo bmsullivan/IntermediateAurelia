2d3a20e0d9f31f42ddb6fe1e9c7f35b8bda2a287
Brian Sullivan <brian@clear-measure.com>
2017-08-07 09:01:02 -0500

    Add navigation translation

--- BoardGames/BoardGames/src/main.js
+++ BoardGames/BoardGames/src/main.js
@@ -2,6 +2,8 @@ import environment from './environment';
 import Backend from 'i18next-xhr-backend';
 import { ValidationMessageProvider } from 'aurelia-validation';
 import { I18N } from 'aurelia-i18n';
+import { Router } from 'aurelia-router';
+import {EventAggregator} from 'aurelia-event-aggregator';
 
 export function configure(aurelia) {
     aurelia.use
@@ -11,13 +13,21 @@ export function configure(aurelia) {
             i18n.i18next.use(Backend);
 
             return i18n.setup({
-                    backend: {
-                        loadPath: '/locales/{{lng}}/{{ns}}.json'
-                    },
-                    lng: 'en',
-                    fallbackLng: 'en',
-                    debug: environment.debug
+                backend: {
+                    loadPath: '/locales/{{lng}}/{{ns}}.json'
+                },
+                lng: 'en',
+                fallbackLng: 'en',
+                debug: environment.debug
+            }).then(() => {
+                const router = aurelia.container.get(Router);
+                const events = aurelia.container.get(EventAggregator);
+
+                router.transformTitle = title => i18n.tr(title);
+                events.subscribe('i18n:locale:changed', () => {
+                    router.updateTitle();
                 });
+            });
         })
         .feature('bootstrap-validation')
         .feature('resources');
